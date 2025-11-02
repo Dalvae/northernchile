21682660e1c653d095d0084053fb8883aed8c9c9
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-11-02 19:28:03 -0300

    added domain

--- backend/src/main/java/com/northernchile/api/config/SecurityConfig.java
+++ backend/src/main/java/com/northernchile/api/config/SecurityConfig.java
@@ -71,7 +71,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://northernchile.com",
+                "https://www.northernchile.com"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin"));
         configuration.setAllowCredentials(true);
