ebde25d181a9c57350e086797b062a105ff0543d
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-11-24 09:31:33 -0300

    fix wrong naming for component

--- frontend/app/components/ui/dock/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Dock } from './Dock.vue'
-export { default as DockIcon } from './DockIcon.vue'
-export { default as DockSeparator } from './DockSeparator.vue'
-
-// Re-export types for convenience
-export * from './types'
