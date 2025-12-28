f5937e3aa9008f4f550c9dde6041588c4afe34a6
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-12-28 15:05:47 -0300

    Add floating WhatsApp button and update contact phone number to +56 9 4269 3271

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -48,6 +48,7 @@ watch(() => consentGiven.value, (hasConsent) => {
     <ClientOnly>
       <CookieBanner />
       <DevWarningModal />
+      <GlobalWhatsAppButton />
     </ClientOnly>
   </div>
 </template>
